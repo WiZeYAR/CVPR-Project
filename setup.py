dd0c3781560e02e4a698e715daee968a473ccb35
Yaroslav Petrov <yaroslav.v.petrov@yandex.ru>
2021-05-13 01:15:56 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cvpr-project",
-    packages=find_packages(include="cvpr"),
+    packages=find_packages(include=["cvpr"]),
     version="0.1.0",
     setup_requires=["pytest-runner"],
     tests_require=["pytest==4.4.1"],
